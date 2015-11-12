90cd2a95c13e9288b69b35c7bc26ff2ef6b735de
Anas Raza Firdousi <anas.firdousi@gmail.com>
2015-11-12 01:45:31 -0800

    Added test spec suit with a sample test using Jasmine,Karma & Browserify * Browserify is required in order to use require() in test for running them in browsers.
    Browserify your test code before sending it to browser if you want to use stuff like 'require()' function. Browserify will provide require() for browser.
    
    To browserify, do the following:
    
     1. npm install browserify --save-dev
     2. Add browserify to your framework field in karma.conf.js
         frameworks: ['jasmine','browserify']
     3. Add browserify to your plugin field in karma.conf.js
         plugins : [
          'karma-chrome-launcher',
          'karma-jasmine',
          'karma-browserify'
        ]
     4.Add browserify to your pre processor field in karma.conf.js
     preprocessors: {
          'src/spec/*.spec.js':['browserify']
        },
    
    Step#4 is v.imp because this is the one which actually puts require() for browsers before sending the test to browsers.

---
+++ src/spec/index.spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Anas on 11/11/2015.
+ */
+
+var con = require('../index.js');
+
+describe('test spec', function(){
+    it('should be true',function(){
+        expect(true).toBe(true);
+    });
+});
+
+
