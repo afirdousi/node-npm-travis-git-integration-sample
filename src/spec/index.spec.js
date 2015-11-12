4055a78f51982e8b09a932dfc1e76920de968cbe
Anas Raza Firdousi <anas.firdousi@gmail.com>
2015-11-12 01:57:28 -0800

    -Added basic configuration test -Added database configuration test

--- src/spec/index.spec.js
+++ src/spec/index.spec.js
@@ -11,3 +11,43 @@ describe('test spec', function(){
 });
 
 
+describe('General Application Configuration', function(){
+
+    it('should have DB configuraiton',function(){
+        expect(con.db).toBeDefined();
+    });
+
+    it('should have Application configuraiton',function(){
+        expect(con.app).toBeDefined();
+    });
+
+    it('should have access to all configuraiton',function(){
+        expect(con.all).toBeDefined();
+    });
+});
+
+describe('Database Configuration', function(){
+
+    it('should have correct Dev DB defined',function(){
+        expect(con.db.dev_url).toBeDefined();
+        expect(con.db.dev_url).toBe('http://dev-db.sampledbserver.com')
+        expect(con.db.dev_status).toBe('U');
+    });
+
+    it('should have correct Pilot DB defined',function(){
+        expect(con.db.pilot_url).toBeDefined();
+        expect(con.db.pilot_url).toBe('http://pilot-db.sampledbserver.com')
+        expect(con.db.pilot_status).toBe('U');
+    });
+
+    it('should have correct Production DB defined',function(){
+        expect(con.db.prod_url).toBeDefined();
+        expect(con.db.prod_url).toBe('http://prod-db.sampledbserver.com')
+        expect(con.db.prod_status).toBe('U');
+    });
+
+});
+
+
+
+
